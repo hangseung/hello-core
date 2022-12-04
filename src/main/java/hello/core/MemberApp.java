a5a28b68e60c39cf26a3fa4258fdb0d6e073f161
hangseung <hangseung.yang@meshkorea.net>
2022-12-04 16:06:26 +0900

    refactor: refactor AppConfig with Spring Annotations
    apply google-java-format

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,18 +3,21 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
-    public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        Member member = new Member(1L, "memberA", Grade.VIP);
-        memberService.join(member);
+  public static void main(String[] args) {
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
-        Member findMember = memberService.findMember(1L);
+    Member member = new Member(1L, "memberA", Grade.VIP);
+    memberService.join(member);
 
-        System.out.println("new member = " + member.getName());
-        System.out.println("find member = " + findMember.getName());
-    }
+    Member findMember = memberService.findMember(1L);
+
+    System.out.println("new member = " + member.getName());
+    System.out.println("find member = " + findMember.getName());
+  }
 }
