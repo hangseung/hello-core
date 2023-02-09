45425c63eb64d93e45a513280134d74032bcab13
hangseung <hangseung.yang@meshkorea.net>
2023-02-09 21:05:13 +0900

    refactor: add @Component annotation to FixDiscountPolicy and add @Primary annotation to RateDiscountPolicy
    add commented codes using @Qualifier annotation

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,16 +2,19 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+// import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+// @Qualifier("mainDiscountPolicy")
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy {
-
-  private int discountPercent = 10;
 
   @Override
   public int discount(Member member, int price) {
     if (member.getGrade() == Grade.VIP) {
+      int discountPercent = 10;
       return price * discountPercent / 100;
     } else {
       return 0;
